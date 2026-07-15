43e1640c4ff32c49d0e2165e92bafd5f60c47124
Abhishek Kumar <iamabhi0619@gmail.com>
2026-07-15 17:02:15 +0530

    firebase admin sdk issue

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -11,7 +11,7 @@ const firebaseAdminSecretSchema = z.object({
 const firebaseAdminSecret = firebaseAdminSecretSchema.parse({
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 });
 
 if (getApps().length === 0) {

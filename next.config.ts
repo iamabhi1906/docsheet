d20465ad47dcfad60036d43ac2217f115bff6024
Abhishek Kumar <iamabhi0619@gmail.com>
2026-07-15 16:52:09 +0530

    admin issue

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["192.168.1.60"],
+  serverExternalPackages: ["firebase-admin"],
 };
 
 export default nextConfig;
